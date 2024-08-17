c7a58d62e63c0ee41579e534ca4ac28075baeaca
ZanTheMan <vanwyk.zander05@gmail.com>
2024-08-17 12:55:37 +0200

    particles

--- GMTK/Assets/meatCook.cs
+++ GMTK/Assets/meatCook.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class meatCook : MonoBehaviour
 {
     public Material[] material;
-    private Renderer rend;
+    public Renderer rend;
+    public GameObject vfx;
+
 
     public int cookTime;
+    private bool isCookig;
+    private bool cookWait;
 
     public bool waited = true;
 
     private void Start()
     {
-        rend = GetComponent<Renderer>();
+     
         rend.sharedMaterial = material[0];
     }
 
-    void OnCollisionStay(Collision collision)
+    void OnTriggerStay(Collider target)
     {
         if ( cookTime == 10)
         {
@@ -34,16 +38,29 @@ public class meatCook : MonoBehaviour
             rend.sharedMaterial = material[3];
         }
 
-
-        if (collision.collider.tag == "Player")
+        if (target.tag != "Stove" && isCookig == true && cookWait == true)
         {
+            
+                StartCoroutine(burning());
+           
+        }
+
+            if (target.tag == "Stove")
+        {
+
+            vfx.gameObject.SetActive(true);
+            isCookig = true;
+            cookWait = false;
+
             Debug.Log("Taged");
             if (waited == true)
             {
                 StartCoroutine(waiter());
 
                 Debug.Log("entering waiter");
-            } 
+            }
+            else
+                StartCoroutine(anotherWaiter());
         }
     }
 
@@ -54,5 +71,19 @@ public class meatCook : MonoBehaviour
         cookTime += 1;
         Debug.Log(cookTime);
         waited = true;
+    }
+
+    IEnumerator burning()
+    {
+        yield return new WaitForSeconds(3);
+        vfx.gameObject.SetActive(false);
+
+        isCookig = false; 
+    }
+
+    IEnumerator anotherWaiter()
+    {
+        yield return new WaitForSeconds(1);
+        cookWait = true;
     }
 }
