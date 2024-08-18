2a816e0b4404068bded8e3307f13ff42a7b7a010
ZanTheMan <vanwyk.zander05@gmail.com>
2024-08-18 16:50:48 +0200

    music

--- GMTK/Assets/Scripts/Button.cs
+++ GMTK/Assets/Scripts/Button.cs
@@ -5,23 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
-    public Canvas pauseCanvas, settingsCanvas;
-    void Start()
-    {
-        settingsCanvas.enabled = false;
-    }
+
 
    public void play()
     {
         SceneManager.LoadScene("Level");
-    }
-    public void Settings()
-    {
-        pauseCanvas.enabled = false;
-        settingsCanvas.enabled = true;
-    }
-    public void Exit()
-    {
-        SceneManager.LoadScene("Main Menu");
     }
 }
