f6a8b525531c8b80ff9f24a3e93cbd293821dfa5
Polylyly <polybius404@gmail.com>
2024-08-20 18:27:12 +0200

    Fix

--- GMTK/Assets/Scripts/Button.cs
+++ GMTK/Assets/Scripts/Button.cs
@@ -5,10 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
+    public Canvas pauseCanvas, settingsCanvas;
 
+    private void Start()
+    {
+        settingsCanvas.enabled = false;
+    }
 
-   public void play()
+    public void play()
     {
         SceneManager.LoadScene("Level");
+    }
+
+    public void Settings()
+    {
+        pauseCanvas.enabled = false;
+        settingsCanvas.enabled = true;
+    }
+
+    public void Exit()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("Main Menu");
     }
 }
